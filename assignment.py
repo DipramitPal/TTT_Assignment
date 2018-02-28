4a4a986ec2537ccbb8e6f544e44e54dd6d1cc38d
Dipramit <dipramit@iitkgp.ac.in>
2018-02-28 21:56:20 +0530

    Deploy ready

---
+++ assignment.py
@@ -0,0 +1,42 @@
+# import requests 
+
+# response = requests.get("http://terriblytinytales.com/test.txt")
+# print(response.content)
+import urllib2
+from flask import jsonify
+from flask import Flask
+from flask import request
+from flask_cors import CORS, cross_origin
+_name_ = '_main_'
+app = Flask(_name_)
+CORS(app)
+
+@app.route('/words',methods = ['POST'])
+def show_list():
+	print (request.headers)
+	# sys.exit()
+	content = request.get_json()
+	try:
+		limit = int(content['count'])
+	except KeyError:
+		limit = 0
+	txt = urllib2.urlopen("http://terriblytinytales.com/test.txt").read()
+	txt = txt.split()
+	frequency = {}
+	for word in txt:
+		word = word.lower()
+		count = frequency.get(word,0)
+		frequency[word] = count +1
+
+	words_list = frequency.keys()
+	frequency_list = []
+	for words in words_list:
+		temp = {}
+		temp['wordName'] = words
+		temp['count'] = frequency[words]
+		frequency_list.append(temp)
+	sorted_list = sorted(frequency_list,key=lambda words: words['count'],reverse=True)
+	return jsonify(sorted_list[:limit])
+
+	
+# print(frequency)
